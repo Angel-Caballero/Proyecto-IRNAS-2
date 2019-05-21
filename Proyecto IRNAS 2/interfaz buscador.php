add636fd94b3c1d7bffee2c73d9701008a5974ac
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2019-05-21 13:01:46 +0200

    buscador_html

---
+++ Proyecto IRNAS 2/interfaz buscador.php
@@ -0,0 +1,16 @@
+<html>
+<section>
+	<form action="" method="GET">		    
+		<input type="search" placeholder="Buscar...">		    	
+        <button type="submit">Buscar</button>
+        <fieldset>
+            <label>
+                <input type="radio" name="tipo" value="Almacenes"> Almacenes
+            </label>
+            <label>
+                <input type="radio" name="tipo" value="Recursos"> Recursos
+            </label>
+        </fieldset>
+	</form>
+</section>
+</html>
