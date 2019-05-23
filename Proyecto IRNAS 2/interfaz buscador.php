53b9a8b2a1e1bcce73c207c90da81012c7ffe3f6
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2019-05-23 12:34:43 +0200

    Update interfaz buscador.php

--- Proyecto IRNAS 2/interfaz buscador.php
+++ Proyecto IRNAS 2/interfaz buscador.php
@@ -1,6 +1,18 @@
-<html>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <link rel="stylesheet" type="text/css" href="css/almacen.css" />
+  <title>Base de Datos: Login</title>
+</head>
+
+<body>
+<?php
+	include_once("cabecera.php");
+?>
 <section>
-	<form action="" method="GET">		    
+	<form action="gestionarBusquedas.php" method="GET">		    
 		<input type="search" placeholder="Buscar...">		    	
         <button type="submit">Buscar</button>
         <fieldset>
@@ -13,4 +25,8 @@
         </fieldset>
 	</form>
 </section>
+<?php
+	include_once("pie.php");
+?>
+</body>
 </html>
