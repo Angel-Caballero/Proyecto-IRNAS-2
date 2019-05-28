776cab07bae6371eef9d2fa637cabe8882b26f29
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-05-28 12:42:47 +0200

    Update interfazBuscador.php

--- Proyecto IRNAS 2/interfazBuscador.php
+++ Proyecto IRNAS 2/interfazBuscador.php
@@ -24,11 +24,12 @@ require_once ("gestionarUsuarios.php");
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <!-- Hay que indicar el fichero externo de estilos -->
-    <link rel="stylesheet" type="text/css" href="css/almacen.css" />
-    <link rel="stylesheet" type="text/css" href="css/menu.css" />
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" type="text/css" href="css/almacen.css" />
+  <link rel="stylesheet" type="text/css" href="css/menu.css" />
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 	<script type="text/javascript" src="./js/menu.js"></script>
   <title>Base de Datos: Buscador</title>
+  <link rel="icon" href="images/icono.png"/>
 </head>
 
 <body>
