c676debf0b9e6e6ab28b83220b4678e7c3cd5383
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2019-05-28 11:18:03 +0200

    Create PantallainfoRecursos.php

---
+++ Proyecto IRNAS 2/PantallainfoRecursos.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" type="text/css" href="css/almacen.css" />
+  <link rel="icon" href="images/icono.png"/>
+  <title>Base de Datos: Login</title>
+</head>
+
+<body>
+<?php
+	include_once("cabecera.php");
+?>
+Nombre: <?php echo ?><br/>
+<br/>
+Posición: <?php echo ?><br/>
+<br/>
+Almacén: <?php echo ?><br/>
+<br/>
+Unidades: <?php echo ?><br/>
+<br/>
+Cantidad: <?php echo ?><br/>
+<br/>
+Reserva Mínima: <?php echo ?><br/>
+<br/>
+Proveedores: <?php echo ?><br/>
+<br/>
+Fórmula Química: <?php echo ?>
+
+</div>
+<?php
+	include_once("pie.php");
+?>
+</body>
+</html>
