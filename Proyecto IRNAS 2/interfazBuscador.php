cece5325e332fe0048cfd8529d93473ee8ad54dc
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-05-28 11:20:54 +0200

    Update interfazBuscador.php

--- Proyecto IRNAS 2/interfazBuscador.php
+++ Proyecto IRNAS 2/interfazBuscador.php
@@ -1,11 +1,26 @@
 <?php
-	session_start();
-  	
-  	include_once("gestionBD.php");
- 	include_once("gestionarUsuarios.php");
-	
-    if(!isset($_SESSION['login'])){
-        Header("Location: login.php");}
+session_start();
+
+require_once ("gestionBD.php");
+require_once ("gestionarBusquedas.php");
+require_once ("gestionarUsuarios.php");
+
+//if (!isset($_SESSION['login'])){
+  //  Header("Location: login.php");
+//}else{
+  //  $usuario = $_SESSION['login'];
+//}
+
+
+	$conexion = crearConexionBD();
+
+    $tipo = consultarTipoUsuario($conexion, $usuario);
+    if($tipo == "ADMINISTRADOR"){
+        $privilegios = true;
+        $_SESSION['privilegios'] = $privilegios;
+    }
+
+	cerrarConexionBD($conexion);
 
 ?>
 
@@ -13,7 +28,11 @@
 <html lang="es">
 <head>
   <meta charset="utf-8">
-  <link rel="stylesheet" type="text/css" href="css/almacen.css" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <!-- Hay que indicar el fichero externo de estilos -->
+    <link rel="stylesheet" type="text/css" href="css/almacen.css" />
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<script type="text/javascript" src="./js/boton.js"></script>
   <title>Base de Datos: Buscador</title>
 </head>
 
@@ -25,14 +44,12 @@
 	<form action="gestionarBusquedas.php" method="GET">	    
 		<input type="search" placeholder="Buscar..." value="search">
         <button type="submit">Buscar</button>
-        <fieldset>
             <label>
                 <input type="radio" name="tipo" value="Almacenes"> Almacenes
             </label>
             <label>
                 <input type="radio" name="tipo" value="Recursos"> Recursos
             </label>
-        </fieldset>
 	</form>
 </section>
 <?php
