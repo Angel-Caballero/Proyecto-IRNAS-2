f17aa736f99d6a2319366c7953d923113b84653d
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-05-28 12:30:55 +0200

    Update logout.php

--- Proyecto IRNAS 2/logout.php
+++ Proyecto IRNAS 2/logout.php
@@ -1,8 +1,12 @@
 <?php
 	session_start();
     
-    if (isset($_SESSION['login']))
+    if (isset($_SESSION['login'])){
         unset($_SESSION['login']);
+    }
+    if(isset($_SESSION['privilegios'])){
     
+    }
+
     header("Location: index.php");
 ?>
