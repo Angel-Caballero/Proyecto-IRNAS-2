b035e751f636e82d33777accd06aed3f806bbaae
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2019-05-29 11:22:15 +0200

    Cambio en el about(pendiente de ser completado)

--- Proyecto IRNAS 2/about.php
+++ Proyecto IRNAS 2/about.php
@@ -10,7 +10,7 @@ session_start()
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" type="text/css" href="css/general.css" />
   <link rel="icon" href="images/icono.png"/>
-  <title>Gestión de biblioteca: Libros</title>
+  <title>Gestión de Almacén: Recursos</title>
 </head>
 
 <body>
@@ -22,24 +22,8 @@ session_start()
 <h3>Información</h2>
 
 <ul>
-<li>Conocer los conceptos básicos de la Ingeniería del Software.</li>
-<li>Conocer los conceptos básicos de los Sistemas de Información.</li>
-<li>Conocer los conceptos básicos de Gestión de Proyectos.</li>
-<li>Conocer los conceptos básicos de Control de Versiones.</li>
-<li>Ser capaz de manejar una herramienta de gestión de proyectos.</li>
-<li>Ser capaz de manejar una herramienta de control de versiones.</li>
-<li>Ser capaz de estudiar un dominio de problema, unos procesos de negocio y elaborar unos requisitos básicos.</li>
-<li>Ser capaz de analizar requisitos mediante el desarrollo de modelos conceptuales.</li>
-<li>Conocer el Modelo Relacional de datos.</li>
-<li>Ser capaz de transformar modelos conceptuales en modelos relacionales.</li>
-<li>Conocer el lenguaje SQL.</li>
-<li>Ser capaz de manejar un SGBD relacional avanzado.</li>
-<li>Ser capaz de desarrollar un esquema SQL complejo.</li>
-<li>Conocer los conceptos básicos de las aplicaciones web.</li>
-<li>Conocer tecnologías de cliente web: HTML, CSS y Javascript.</li>
-<li>Ser capaz de desarrollar una interfaz de usuario sencilla con HTML, CSS y Javascript.</li>
-<li>Conocer tecnologías de procesamiento en servidor web.</li>
-<li>Ser capaz de desarrollar una aplicación web con acceso a una base de datos relacional.</li>
+<li>Esto es una pagina web de gestión de recursos</li>
+<li>Diseñada para el centro de Investigacion IRNAS</li>
 </ul>
 
 
