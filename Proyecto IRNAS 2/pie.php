ade88be39da15533ebaf23da805ab7f9475426a9
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-05-29 12:48:14 +0200

    Cambio Drastico en pie.php

--- Proyecto IRNAS 2/pie.php
+++ Proyecto IRNAS 2/pie.php
@@ -1,6 +1,6 @@
 <footer>
 	<div style="height:100%; width:100%" class="centrado">
-	<span><a href="about.php" style="color:white;">Informacion</a>
+	<span><a href="about.php" style="color:white;">Información</a>
 	Copyright &copy; 2019 IRNAS (CSIC) - Todos los derechos reservados</span>
 </div>
 </footer>
