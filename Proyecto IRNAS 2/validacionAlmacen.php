3458e41fad37684f2f24a176db44a21b49bc820d
FranVargasCastro <50156906+FranVargasCastro@users.noreply.github.com>
2019-05-31 12:42:54 +0200

    cambios varios

---
+++ Proyecto IRNAS 2/validacionAlmacen.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+if (isset($_SESSION["formularioAlmacen"])) {
+    $nuevoAlmacen["nombre"] = $_REQUEST["nombre"];
+    $nuevoAlmacen["tipo-iluminacion"] = $_REQUEST["tipo-iluminacion"];
+    $nuevoAlmacen["temperatura"] = $_REQUEST["temperatura"];
+    $nuevoAlmacen["tipo-camara"] = $_REQUEST["tipo-camara"];
+    $_SESSION["formularioAlmacen"] = $nuevoAlmacen;		
+}
+
+else 
+    Header("Location: form_alta_usuario.php");
+
+    $conexion = crearConexionBD(); 
+	$errores = validarDatosUsuario($conexion, $nuevoAlmacen);
+    cerrarConexionBD($conexion);
+    
+if (count($errores)>0) {
+	
+	$_SESSION["errores"] = $errores;
+	Header('Location: form_alta_usuario.php');
+} else
+	
+    Header('Location: accion_alta_usuario.php');
+        
+function validarDatosUsuario($conexion, $nuevoAlmacen){
+    $errores=array();
+
+    if($nuevoAlmacen["nombre"]=="") 
+    $errores[] = "<p>El nombre no puede estar vacío</p>";
+
+    if($nuevoAlmacen["tipo-iluminacion"]==""){ 
+		$errores[] = "<p>El tipo-iluminacion no puede estar vacío</p>";
+	}else if(!isset($nuevoAlmacen["tipo-iluminacion"])){
+        $expresion = '/^[a-z]$/'
+            if(preg_match($expresion, $value)){ echo 'El tipo de iluminacion es correcto'; }else{$errores[] = "<p> El tipo de iluminacion no puede tener numeros</p>"; })
+?>
--- Proyecto IRNAS 2/validacionProveedor.php
+++ Proyecto IRNAS 2/validacionProveedor.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
-if (isset($_SESSION["proveedor"])) {
+if (isset($_SESSION["formularioProveedor"])) {
     $nuevoProveedor["nombre-empresa"] = $_REQUEST["nombre_empresa"];
     $nuevoProveedor["nombre-comercial"] = $_REQUEST["nombre-comercial"];
     $nuevoProveedor["email"] = $_REQUEST["email"];
     $nuevoProveedor["telefono1"] = $_REQUEST["telefono1"];
     $nuevoProveedor["telefono2"] = $_REQUEST["telefono2"];
     $nuevoProveedor["telefono3"] = $_REQUEST["telefono3"]
-    $_SESSION["proveedor"] = $nuevoProveedor;		
+    $_SESSION["formularioProveedor"] = $nuevoProveedor;		
 }
 
 else 
@@ -20,7 +20,7 @@ else
 if (count($errores)>0) {
 	
 	$_SESSION["errores"] = $errores;
-	Header('Location: form_alta_usuario.php');
+	Header('añadir.php');
 } else
 	
     Header('Location: accion_alta_usuario.php');
@@ -36,18 +36,18 @@ function validarDatosProveedor($conexion, $nuevoProveedor){
 	}else if(!filter_var($nuevoProveedor["email"], FILTER_VALIDATE_EMAIL)){
         $errores[] = "<p>El email es incorrecto: " . $nuevoUsuario["email"]. "</p>";
         
-    if(!isset($nuevoProveedor["telefono1"]) || 
-    $expresion = '/^[9|6|7][0-9]{8}$/'){
-        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ echo 'El telefono es incorrecto'; })
+    if(!isset($nuevoProveedor["telefono1"])){
+    $expresion = '/^[9|6|7][0-9]{8}$/'
+        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ $errores[] = "<p>El telefono introducido no es valido</p>"; })
     }else if($nuevoProveedor["telefono1"]==""){
         $errores[] = "<p>El telefono1 no puede estar vacío</p>";
     }
-    if(!isset($nuevoProveedor["telefono2"]) || 
-    $expresion = '/^[9|6|7][0-9]{8}$/'){
-        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ echo 'El telefono es incorrecto'; })
-
-    if(!isset($nuevoProveedor["telefono3"]) || 
-    $expresion = '/^[9|6|7][0-9]{8}$/'){
-        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ echo 'El telefono es incorrecto'; })
-     
+    if(!isset($nuevoProveedor["telefono2"])){
+    $expresion = '/^[9|6|7][0-9]{8}$/'
+        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ $errores[] = "<p>El telefono introducido no es valido</p>"; })
+    }
+    if(!isset($nuevoProveedor["telefono3"])){ 
+    $expresion = '/^[9|6|7][0-9]{8}$/'
+        if(preg_match($expresion, $value)){ echo 'El telefono es correcto'; }else{ $errores[] = "<p>El telefono introducido no es valido</p>"; })
+    }
 ?>
