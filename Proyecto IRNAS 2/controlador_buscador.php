32b70675c91b2f29a2c82ce778ba0296166474e0
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-05-31 12:49:49 +0200

    Update controlador_buscador.php

--- Proyecto IRNAS 2/controlador_buscador.php
+++ Proyecto IRNAS 2/controlador_buscador.php
@@ -3,14 +3,14 @@
 	if(isset($_REQUEST["tipo"])){
         $tipo = $_REQUEST["tipo"];
         if ($tipo == "Almacen"){
-            $almacen["search"] = $_REQUEST["search"];
+            $almacen = $_REQUEST["search"];
             
             $_SESSION["almacen"] = $almacen;
                 
             Header("Location: listaResultados.php"); 
         }
         elseif ($tipo == "Recurso") {
-            $recurso["search"] = $_REQUEST["search"];
+            $recurso = $_REQUEST["search"];
             
             $_SESSION["recurso"] = $recurso;
             Header("Location: listaResultados.php");
