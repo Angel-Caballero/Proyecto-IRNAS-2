6a0eedabc5ede403ca9e0d6bc96ddd0dda6372b4
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-08-29 16:50:16 +0200

    Update Insercion de Almacenes y Recursos.sql

--- Proyecto IRNAS 2/sql/Insercion de Almacenes y Recursos.sql
+++ Proyecto IRNAS 2/sql/Insercion de Almacenes y Recursos.sql
@@ -2,7 +2,7 @@ BEGIN
 
 INSERTAR_ALMACENES('Sotano', 22, '12W', 'NORMAL');
 INSERTAR_ALMACENES('Invernadero', 24, 'Natural', 'NORMAL');
-INSERTAR_ALMACENES('Camara Frigorifica A', -12, '10W4B', 'CAMARA FRIO');
+INSERTAR_ALMACENES('Camara Frigorifica A', -10, '10W4B', 'CAMARA FRIO');
 INSERTAR_ALMACENES('Camara in-vitro', 19, '4B23R', 'CAMARA IN-VITRO');
 
 COMMIT;
