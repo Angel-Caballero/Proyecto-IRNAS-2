7e58b01ee82666479ab7b9bc07a1ce41056e2888
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-08-31 17:39:40 +0200

    Update gestionarUsuarios.php

--- Proyecto IRNAS 2/gestionarUsuarios.php
+++ Proyecto IRNAS 2/gestionarUsuarios.php
@@ -1,10 +1,4 @@
 <?php
-  /*
-     * #===========================================================#
-     * #	Este fichero contiene las funciones de gestión
-     * #	de usuarios de la capa de acceso a datos
-     * #==========================================================#
-     */
 
  function alta_usuario($conexion,$usuario) {
 
