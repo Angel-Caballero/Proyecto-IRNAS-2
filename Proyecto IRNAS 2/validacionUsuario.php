7d3f3eb94d439df7a3dc27be6008111ce4a7162a
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-09-01 17:28:08 +0200

    Update validacionUsuario.php

--- Proyecto IRNAS 2/validacionUsuario.php
+++ Proyecto IRNAS 2/validacionUsuario.php
@@ -33,17 +33,19 @@ if (count($errores)>0) {
 function validarDatosUsuario($conexion, $nuevoUsuario){
     $errores=array();
 
-    if($nuevoUsuario["nombre"]=="") 
+    if($nuevoUsuario["nombre"]==""){
     $errores[] = "<p>El nombre no puede estar vacío</p>";
+    }
 
     if($nuevoUsuario["email"]==""){ 
 		$errores[] = "<p>El email no puede estar vacío</p>";
 	}else if(!filter_var($nuevoUsuario["email"], FILTER_VALIDATE_EMAIL)){
         $errores[] = "<p>El email es incorrecto: " . $nuevoUsuario["email"]. "</p>";
-        
+    }
+
     if(!isset($nuevoUsuario["pass"]) || strlen($nuevoUsuario["pass"])<8){
-        $errores [] = "<p>Contraseña no válida: debe tener al menos 8 caracteres</p>";
-    }else if(!preg_match("/[a-z]+/", $nuevoUsuario["pass"]) || !preg_match("/[A-Z]+/", $nuevoUsuario["pass"]){
+        $errores[] = "<p>Contraseña no válida: debe tener al menos 8 caracteres</p>";
+    }else if(!preg_match("/[a-z]+/", $nuevoUsuario["pass"]) || !preg_match("/[A-Z]+/", $nuevoUsuario["pass"])){
         $errores[] = "<p>Contraseña no válida: debe contener letras mayúsculas y minúsculas</p>";
     }else if(strlen($nuevoUsuario["pass"]) > 16){
         $errores[] = "<p>Contraseña no válida: no puede tener más de 16 caracteres</p>";
@@ -52,4 +54,6 @@ function validarDatosUsuario($conexion, $nuevoUsuario){
     if($nuevoUsuario["tipo"] != "TRABAJADOR" && $nuevoUsuario["tipo"] != "ADMINISTRADOR"){
         $errores[] = "<p>El tipo de usuario debe ser: TRABAJADOR o ADMINISTRADOR</p>";
     }
+}
+
 ?>
