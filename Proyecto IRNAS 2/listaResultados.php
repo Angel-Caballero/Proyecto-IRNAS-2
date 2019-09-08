771fccedb967abbcdf6f2aff52155a0d24bd41bb
Angel-Caballero <49127396+Angel-Caballero@users.noreply.github.com>
2019-09-08 10:54:31 +0200

    Terminada paginacion

--- Proyecto IRNAS 2/listaResultados.php
+++ Proyecto IRNAS 2/listaResultados.php
@@ -16,6 +16,26 @@ if (isset($_SESSION["recurso"])) {
 	unset($_SESSION["recurso"]);
 }
 
+// ¿Venimos simplemente de cambiar página o de haber seleccionado un registro ?
+// ¿Hay una sesión activa?
+if (isset($_SESSION["paginacion"])) {
+	$paginacion = $_SESSION["paginacion"];
+}
+
+$pagina_seleccionada = isset($_GET["PAG_NUM"]) ? (int) $_GET["PAG_NUM"] : (isset($paginacion) ? (int) $paginacion["PAG_NUM"] : 1);
+$pag_tam = isset($_GET["PAG_TAM"]) ? (int) $_GET["PAG_TAM"] : (isset($paginacion) ? (int) $paginacion["PAG_TAM"] : 5);
+
+if ($pagina_seleccionada < 1) {
+	$pagina_seleccionada = 1;
+}
+
+if ($pag_tam < 1) {
+	$pag_tam = 5;
+}
+
+// Antes de seguir, borramos las variables de sección para no confundirnos más adelante
+unset($_SESSION["paginacion"]);
+
 $conexion = crearConexionBD();
 
 if (isset($_GET["almacen"])) {
@@ -24,13 +44,63 @@ if (isset($_GET["almacen"])) {
 	$recursos = recursosEnAlmacen($conexion, $query);
 } else {
 	if (isset($_SESSION['busqRecurso'])) {
-		$query = $_SESSION['busqRecurso'];
-		unset($_SESSION['busqAlmacen']);
-		$recursos = buscarRecursos($conexion, $query);
+		if ($_SESSION['busqRecurso'] == "") {
+			// La consulta que ha de paginarse
+			$query = 'SELECT * FROM RECURSOS WHERE (NOMBRE = NOMBRE) ORDER BY NOMBRE';
+
+			// Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
+			// En caso de que no, se asume el tamaño de página propuesto, pero desde la página 1
+			$total_registros = total_consulta($conexion, $query);
+			$total_paginas = (int) ($total_registros / $pag_tam);
+
+			if ($total_registros % $pag_tam > 0) {
+				$total_paginas++;
+			}
+
+			if ($pagina_seleccionada > $total_paginas) {
+				$pagina_seleccionada = $total_paginas;
+			}
+
+			// Generamos los valores de sesión para página e intervalo para volver a ella después de una operación
+			$paginacion["PAG_NUM"] = $pagina_seleccionada;
+			$paginacion["PAG_TAM"] = $pag_tam;
+			$_SESSION["paginacion"] = $paginacion;
+
+			$filasRecursos = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
+		} else {
+			$query = $_SESSION['busqRecurso'];
+			unset($_SESSION['busqAlmacen']);
+			$recursos = buscarRecursos($conexion, $query);
+		}
 	} else if (isset($_SESSION['busqAlmacen'])) {
-		$query = $_SESSION['busqAlmacen'];
-		unset($_SESSION['busqRecurso']);
-		$almacenes = buscarAlmacenes($conexion, $query);
+		if ($_SESSION['busqAlmacen'] == "") {
+			// La consulta que ha de paginarse
+			$query = 'SELECT * FROM ALMACENES WHERE (NOMBRE = NOMBRE) ORDER BY NOMBRE';
+
+			// Se comprueba que el tamaño de página, página seleccionada y total de registros son conformes.
+			// En caso de que no, se asume el tamaño de página propuesto, pero desde la página 1
+			$total_registros = total_consulta($conexion, $query);
+			$total_paginas = (int) ($total_registros / $pag_tam);
+
+			if ($total_registros % $pag_tam > 0) {
+				$total_paginas++;
+			}
+
+			if ($pagina_seleccionada > $total_paginas) {
+				$pagina_seleccionada = $total_paginas;
+			}
+
+			// Generamos los valores de sesión para página e intervalo para volver a ella después de una operación
+			$paginacion["PAG_NUM"] = $pagina_seleccionada;
+			$paginacion["PAG_TAM"] = $pag_tam;
+			$_SESSION["paginacion"] = $paginacion;
+
+			$filasAlmacenes = consulta_paginada($conexion, $query, $pagina_seleccionada, $pag_tam);
+		} else {
+			$query = $_SESSION['busqAlmacen'];
+			unset($_SESSION['busqRecurso']);
+			$almacenes = buscarAlmacenes($conexion, $query);
+		}
 	} else {
 		Header("Location: interfazBuscador.php");
 	}
@@ -62,54 +132,132 @@ cerrarConexionBD($conexion);
 			<?php
 			include_once("menu.php");
 			?>
+			<?php if (isset($filasRecursos) || isset($filasAlmacenes)) { ?>
+				<nav>
 
+					<div id="enlaces">
+
+						<?php
+
+							for ($pagina = 1; $pagina <= $total_paginas; $pagina++)
+
+								if ($pagina == $pagina_seleccionada) { 	?>
+
+							<span class="current"><?php echo $pagina; ?></span>
+
+						<?php } else { ?>
+
+							<a href="listaResultados.php?PAG_NUM=<?php echo $pagina; ?>&PAG_TAM=<?php echo $pag_tam; ?>"><?php echo $pagina; ?></a>
+
+						<?php } ?>
+
+					</div>
+
+
+
+					<form method="get" action="listaResultados.php">
+
+						<input id="PAG_NUM" name="PAG_NUM" type="hidden" value="<?php echo $pagina_seleccionada ?>" />
+
+						Mostrando
+
+						<input id="PAG_TAM" name="PAG_TAM" type="number" min="1" max="<?php echo $total_registros; ?>" value="<?php echo $pag_tam ?>" autofocus="autofocus" />
+
+						entradas de <?php echo $total_registros ?>
+
+						<input type="submit" value="Cambiar">
+
+					</form>
+
+				</nav>
+			<?php } ?>
 			<table style="width:90%;">
 				<tr>
 					<?php if (isset($_SESSION["busqAlmacen"])) { ?>
 						<th>Almacen</th>
 						<th>Tipo de camara</th>
 						<th>Temperatura</th>
-						<?php foreach ($almacenes as $almacen) { ?>
+						<?php if (isset($filasAlmacenes)) {
+							foreach ($filasAlmacenes as $almacen) { ?>
 				<tr>
 					<td><a href="listaResultados.php?almacen=<?php echo $almacen["NOMBRE"]; ?>"><?php echo $almacen["NOMBRE"]; ?></td>
 					<td><?php echo $almacen["TIPOCAMARA"]; ?></td>
 					<td><?php echo $almacen["TEMPERATURA"]; ?></td>
 				</tr>
-			<?php } ?>
-		<?php } else if (isset($_SESSION["busqRecurso"]) || isset($_GET["almacen"])) { ?>
-			<tr>
-			<th>Recurso</th>
-			<th>Almacen</th>
-			<th>Tipo</th>
-			<?php foreach ($recursos as $recurso) { ?>
+							<?php } ?>
+						<?php } else {
+							foreach ($almacenes as $almacen) { ?>
 				<tr>
-					<td><?php echo $recurso["NOMBRE"]; ?></td>
-					<td><?php echo $recurso["ALMACEN"]; ?></td>
-					<td><?php echo $recurso["TIPO"]; ?></td>
-					<td><form method="post" action="controlador_recursos.php">
-							<div class="fila_recurso">
-								<div class="datos_recurso">
-								<form method="post" action="accion_borrar_recurso.php">
-									<input id="NOMBRE" name="NOMBRE" type="hidden" value="<?php echo $recurso["NOMBRE"]; ?>" />
-									<input id="FORMULAQUIMICA" name="FORMULAQUIMICA" type="hidden" value="<?php echo $recurso["FORMULAQUIMICA"]; ?>" />
-									<input id="UNIDADES" name="UNIDADES" type="hidden" value="<?php echo $recurso["UNIDADES"]; ?>" />
-									<input id="CANTIDAD" name="CANTIDAD" type="hidden" value="<?php echo $recurso["CANTIDAD"]; ?>" />
-									<input id="RESERVAMINIMA" name="RESERVAMINIMA" type="hidden" value="<?php echo $recurso["RESERVAMINIMA"]; ?>" />
-									<input id="TIPO" name="TIPO" type="hidden" value="<?php echo $recurso["TIPO"]; ?>" />
-									<input id="ALMACEN" name="ALMACEN" type="hidden" value="<?php echo $recurso["ALMACEN"]; ?>" />
-								</div>
-								<div id="boton">
-									<input type="submit" name="info" id="info" value="Informacion"/>
-								</div>
-							</div>
-						</form>
-					</td>
+					<td><a href="listaResultados.php?almacen=<?php echo $almacen["NOMBRE"]; ?>"><?php echo $almacen["NOMBRE"]; ?></td>
+					<td><?php echo $almacen["TIPOCAMARA"]; ?></td>
+					<td><?php echo $almacen["TEMPERATURA"]; ?></td>
 				</tr>
-			<?php } ?>
-		<?php } ?>
-		</tr>
-		</table>
-	</div>
+							<?php } ?>
+						<?php } ?>
+					<?php } else if (isset($_SESSION["busqRecurso"]) || isset($_GET["almacen"])) { ?>
+							<tr>
+								<th>Recurso</th>
+								<th>Almacen</th>
+								<th>Tipo</th>
+								<?php if (isset($filasRecursos)) {
+									foreach ($filasRecursos as $recurso) { ?>
+							<tr>
+										<td><?php echo $recurso["NOMBRE"]; ?></td>
+										<td><?php echo $recurso["ALMACEN"]; ?></td>
+										<td><?php echo $recurso["TIPO"]; ?></td>
+										<td>
+										<form method="post" action="controlador_recursos.php">
+											<div class="fila_recurso">
+												<div class="datos_recurso">
+													<form method="post" action="accion_borrar_recurso.php">
+														<input id="NOMBRE" name="NOMBRE" type="hidden" value="<?php echo $recurso["NOMBRE"]; ?>" />
+														<input id="FORMULAQUIMICA" name="FORMULAQUIMICA" type="hidden" value="<?php echo $recurso["FORMULAQUIMICA"]; ?>" />
+														<input id="UNIDADES" name="UNIDADES" type="hidden" value="<?php echo $recurso["UNIDADES"]; ?>" />
+														<input id="CANTIDAD" name="CANTIDAD" type="hidden" value="<?php echo $recurso["CANTIDAD"]; ?>" />
+														<input id="RESERVAMINIMA" name="RESERVAMINIMA" type="hidden" value="<?php echo $recurso["RESERVAMINIMA"]; ?>" />
+														<input id="TIPO" name="TIPO" type="hidden" value="<?php echo $recurso["TIPO"]; ?>" />
+														<input id="ALMACEN" name="ALMACEN" type="hidden" value="<?php echo $recurso["ALMACEN"]; ?>" />
+												</div>
+												<div id="boton">
+													<input type="submit" name="info" id="info" value="Informacion" />
+												</div>
+											</div>
+										</form>
+										</td>
+							</tr>
+									<?php } ?>
+								<?php } else {
+										foreach ($recursos as $recurso) { ?>
+									<tr>
+											<td><?php echo $recurso["NOMBRE"]; ?></td>
+											<td><?php echo $recurso["ALMACEN"]; ?></td>
+											<td><?php echo $recurso["TIPO"]; ?></td>
+											<td>
+												<form method="post" action="controlador_recursos.php">
+													<div class="fila_recurso">
+														<div class="datos_recurso">
+															<form method="post" action="accion_borrar_recurso.php">
+																<input id="NOMBRE" name="NOMBRE" type="hidden" value="<?php echo $recurso["NOMBRE"]; ?>" />
+																<input id="FORMULAQUIMICA" name="FORMULAQUIMICA" type="hidden" value="<?php echo $recurso["FORMULAQUIMICA"]; ?>" />
+																<input id="UNIDADES" name="UNIDADES" type="hidden" value="<?php echo $recurso["UNIDADES"]; ?>" />
+																<input id="CANTIDAD" name="CANTIDAD" type="hidden" value="<?php echo $recurso["CANTIDAD"]; ?>" />
+																<input id="RESERVAMINIMA" name="RESERVAMINIMA" type="hidden" value="<?php echo $recurso["RESERVAMINIMA"]; ?>" />
+																<input id="TIPO" name="TIPO" type="hidden" value="<?php echo $recurso["TIPO"]; ?>" />
+																<input id="ALMACEN" name="ALMACEN" type="hidden" value="<?php echo $recurso["ALMACEN"]; ?>" />
+														</div>
+														<div id="boton">
+															<input type="submit" name="info" id="info" value="Informacion" />
+														</div>
+													</div>
+												</form>
+											</td>
+									</tr>
+										<?php } ?>
+									<?php } ?>
+					<?php } ?>
+
+			</table>
+		</div>
 	</div>
 	<?php
 	include_once("pie.php");
